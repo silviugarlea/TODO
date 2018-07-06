839b5479ae95e78daa710e91bf9ee08e474ce229
Silviu Garlea <garlea.silviu@gmail.com>
2018-07-06 16:19:40 +0300

    solved feign client issue

--- task-service/src/main/java/com/sgarlea/todo/client/NotificationClient.java
+++ task-service/src/main/java/com/sgarlea/todo/client/NotificationClient.java
@@ -1,15 +1,13 @@
 package com.sgarlea.todo.client;
 
 import com.sgarlea.todo.domain.Task;
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@FeignClient(name = "task-service")
-@RibbonClient(name = "task-service")
+@FeignClient(value = "notification-service")
 @RequestMapping(path = "/notificationservice", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
 public interface NotificationClient {
 
@@ -17,15 +15,15 @@ public interface NotificationClient {
     Object createNotification(Task task);
 
     @RequestMapping(method = RequestMethod.PUT, path = "/notifications/{taskId}")
-    Object updateNotification(@PathVariable String taskId, Task task);
+    Object updateNotification(@PathVariable("taskId") String taskId, Task task);
 
     @RequestMapping(method = RequestMethod.DELETE, path = "/notifications/{taskId}")
-    void deleteNotification(@PathVariable String taskId);
+    void deleteNotification(@PathVariable("taskId") String taskId);
 
     @RequestMapping(method = RequestMethod.PUT, path = "/notifications/{taskId}/activate")
-    Object activateNotification(@PathVariable String taskId);
+    Object activateNotification(@PathVariable("taskId") String taskId);
 
     @RequestMapping(method = RequestMethod.PUT, path = "/notifications/{taskId}/deactivate")
-    Object deactivateNotification(@PathVariable String taskId);
+    Object deactivateNotification(@PathVariable("taskId") String taskId);
 
 }
