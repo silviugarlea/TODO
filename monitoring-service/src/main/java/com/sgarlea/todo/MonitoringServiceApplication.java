bf43da09f82ad0c535bdd16617e49e38b2d1a771
Silviu Garlea <garlea.silviu@gmail.com>
2018-08-02 10:40:07 +0300

    turbine?

--- monitoring-service/src/main/java/com/sgarlea/todo/MonitoringServiceApplication.java
+++ monitoring-service/src/main/java/com/sgarlea/todo/MonitoringServiceApplication.java
@@ -3,9 +3,11 @@ package com.sgarlea.todo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.turbine.stream.EnableTurbineStream;
 
 @SpringBootApplication
 @EnableHystrixDashboard
+@EnableTurbineStream
 public class MonitoringServiceApplication {
 
     public static void main(String[] args) {
